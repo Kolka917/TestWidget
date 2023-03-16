4b10f26cd5ef71660e95206b09cfd278226e74b5
Kolka917 <88964476+Kolka917@users.noreply.github.com>
2023-03-16 17:12:07 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,9 +9,7 @@ const testURL = 'https://api.ati.su/v1.0/dictionaries/cities/240';
 const myInit = {
     method: 'GET',
     headers: {
-        'method': 'GET',
-        'Access-Control-Allow-Origin': 'http://localhost:63342/',
-        'origin': 'http://localhost:63342/'
+        'method': 'GET'
     }
 
 };
