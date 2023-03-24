d180f3811df842dcb775475f8983011c8f419846
Kolka917 <88964476+Kolka917@users.noreply.github.com>
2023-03-24 17:58:05 +0300

    Update script.js

--- chooseLoadingTypeWidget/script.js
+++ chooseLoadingTypeWidget/script.js
@@ -1,51 +1,60 @@
 $(document).ready(function () {
-            $.ajax({
-                url: "https://proxy.cors.sh/https://api.ati.su/v1.0/dictionaries/loadingTypes",
-                headers: {
-                    'x-cors-api-key': 'temp_96f3dd2cacb5a4a98c8728b253decd48'
-                },
-                contentType: 'application/json',
-                type: 'GET',
-                success: function (data, status) {
-                    if (status === "success") {
-                        let search_result = document.getElementById('search_box-result');
-                            let table = '<table><tr><th>Наименование</th><th>Id для отправки в ATI</th></tr>';
+    $.ajax({
+        url: "https://proxy.cors.sh/https://api.ati.su/v1.0/dictionaries/carTypes",
+        headers: {
+            'x-cors-api-key': 'temp_96f3dd2cacb5a4a98c8728b253decd48'
+        },
+        contentType: 'application/json',
+        type: 'GET',
+        success: function (data, status) {
+            if (status === "success") {
+                console.log("Post successfully created!");
+                let search_result = document.getElementById('search_box-result-car-type');
 
-                            data.forEach(function (d) {
-                                table +=
-                                    "<tr>\n" +
-                                    "<td className=\"search_result-name\">\n" +
-                                    "	<a href=\"#\">" + d.Name + "</a>\n" +
-                                    "</td>\n" +
-                                    "<td className=\"search_result-name\">\n" +
-                                    "	<a href=\"#\">" + d.Id + "</a>\n" +
-                                    "</td>\n" +
-                                    "</tr>";
-                            })
-                            table += '</table>';
+                let table = '<div class="btn-group">\n' +
+                    '    <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">\n' +
+                    '        ATI.Список типов кузовов c ID\n' +
+                    '    </button>\n' +
+                    '    <ul class="dropdown-menu">';
 
-                            search_result.innerHTML = table;
-                    }
-                }
-            });
+                data.forEach(function (d) {
+                    table += '<li><a class="dropdown-item" href="#">' + d.Name + '(' + d.TypeId + ')' + '</a></li>';
+                })
+                table += '</ul>\n' +
+                    '</div>';
 
-    $(document).on('click', function (e) {
-        if (!$(e.target).closest('.search_box').length) {
-            $result.html('');
-            $result.fadeOut(100);
+                search_result.innerHTML = table;
+            }
         }
     });
 
-    $(document).on('click', '.search_result-name a', function () {
-        $('#search').val($(this).text());
-        $result.fadeOut(100);
-        return false;
-    });
 
-    $(document).on('click', function (e) {
-        if (!$(e.target).closest('.search_box').length) {
-            $result.html('');
-            $result.fadeOut(100);
+    $.ajax({
+        url: "https://proxy.cors.sh/https://api.ati.su/v1.0/dictionaries/loadingTypes",
+        headers: {
+            'x-cors-api-key': 'temp_96f3dd2cacb5a4a98c8728b253decd48'
+        },
+        contentType: 'application/json',
+        type: 'GET',
+        success: function (data, status) {
+            if (status === "success") {
+                console.log("Post successfully created!");
+                let search_result = document.getElementById('search_box-result-loading-type');
+
+                let table = '<div class="btn-group">\n' +
+                    '    <button type="button" class="btn btn-success btn-sm  dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">\n' +
+                    '        ATI.Список типов погрузки-разгрузки c ID\n' +
+                    '    </button>\n' +
+                    '    <ul class="dropdown-menu">';
+
+                data.forEach(function (d) {
+                    table += '<li><a class="dropdown-item" href="#">' + d.Name + '(' + d.Id + ')' + '</a></li>';
+                })
+                table += '</ul>\n' +
+                    '</div>';
+
+                search_result.innerHTML = table;
+            }
         }
     });
 
