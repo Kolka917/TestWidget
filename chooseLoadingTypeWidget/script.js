c4cae0ba9b4f26b96f11d6e91f78777d9bb7ed4c
Kolka917 <88964476+Kolka917@users.noreply.github.com>
2023-03-24 06:38:57 +0300

    Create script.js

---
+++ chooseLoadingTypeWidget/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+            $.ajax({
+                url: "https://proxy.cors.sh/https://api.ati.su/v1.0/dictionaries/carTypes",
+                headers: {
+                    'x-cors-api-key': 'temp_96f3dd2cacb5a4a98c8728b253decd48'
+                },
+                contentType: 'application/json',
+                type: 'GET',
+                success: function (data, status) {
+                    if (status === "success") {
+                        let search_result = document.getElementById('search_box-result');
+                            let table = '<table><tr><th>Наименование</th><th>Id для отправки в ATI</th></tr>';
+
+                            data.forEach(function (d) {
+                                table +=
+                                    "<tr>\n" +
+                                    "<td className=\"search_result-name\">\n" +
+                                    "	<a href=\"#\">" + d.Name + "</a>\n" +
+                                    "</td>\n" +
+                                    "<td className=\"search_result-name\">\n" +
+                                    "	<a href=\"#\">" + d.TypeId + "</a>\n" +
+                                    "</td>\n" +
+                                    "</tr>";
+                            })
+                            table += '</table>';
+
+                            search_result.innerHTML = table;
+                    }
+                }
+            });
+
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest('.search_box').length) {
+            $result.html('');
+            $result.fadeOut(100);
+        }
+    });
+
+    $(document).on('click', '.search_result-name a', function () {
+        $('#search').val($(this).text());
+        $result.fadeOut(100);
+        return false;
+    });
+
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest('.search_box').length) {
+            $result.html('');
+            $result.fadeOut(100);
+        }
+    });
+
+});
