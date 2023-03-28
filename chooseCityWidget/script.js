3b09e85312dbde6107afb26eae8fc0050847ce4c
nrodionov <nrodionov@i-novus.ru>
2023-03-28 07:17:28 +0300

    Put id into field of deal

--- chooseCityWidget/script.js
+++ chooseCityWidget/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+    $.get({
+        url: "https://proxy.cors.sh/https://logisticplus.envycrm.com/openapi/v1/deal/getListInputs/?api_key=44ac90e848a1b3a3efd692e259461c47e8405b5f&type=custom",
+        headers: {
+            'x-cors-api-key': 'temp_96f3dd2cacb5a4a98c8728b253decd48'
+        },
+        contentType: 'application/json',
+        type: 'GET',
+        success: function (data, status) {
+            if (status === "success") {
+                let fieldId = document.getElementById('fieldId');
+                let fieldIdData = '';
+                Array.prototype.forEach.call(data.result, d => {
+                    if (d.name_id === "crm_582598") {
+                        fieldIdData += d.id
+                    }
+                })
+                fieldId.innerHTML = fieldIdData;
+            }
+        }
+    })
+
+
     $('#search').on('keyup', function () {
         var search = $(this).val();
         if ((search != '') && (search.length > 2)) {
@@ -14,14 +36,18 @@ $(document).ready(function () {
                         let search_result = document.getElementById('search_box-result');
                         if (data.length > 1) {
                             let table = '<form>' +
-                            '<select class="search_output" size="6"  multiple>';
+                                '<select id="selectCity" class="search_output" size="6">';
 
-                        data.forEach(function (d) {
-                            table += '<option>' + d.FullName + '(' + d.CityId + ')';
-                        })
-                        table += '</select></form>';
+                            data.forEach(function (d) {
+                                table += '<option value="' + d.CityId + '"' + '>' + d.FullName + '(' + d.CityId + ')';
+                            })
+                            table += '</select>' +
+                                '<input type="button" onclick="display()" value="Выбрать город!">'
+                            '</form>';
 
-                        search_result.innerHTML = table;
+                            search_result.innerHTML = table;
+
+
                         } else {
                             let notFoundResult = '<p>Не найдено города с указанным названием. Если вы уверены, что название верное, попробуйте ввести его еще раз.</p>';
                             search_result.innerHTML = notFoundResult;
@@ -29,6 +55,8 @@ $(document).ready(function () {
                     }
                 }
             })
+
+
         } else {
             $result.html('');
             $result.fadeOut(100);
