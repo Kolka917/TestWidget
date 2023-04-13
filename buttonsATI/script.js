d05168676bce831273353bc4e85f36b51b0518c4
nrodionov <nrodionov@i-novus.ru>
2023-04-13 11:24:47 +0300

    parse values from deal

--- buttonsATI/script.js
+++ buttonsATI/script.js
@@ -119,7 +119,7 @@ function createCargo() {
                 let paymentWithVatValue= getValue(paymentWithVatFieldId)
                 let paymentWithoutVatValue= getValue(paymentWithoutVatFieldId)
                 let contactsIdValue= getValue(contactsIdFieldId)
-                console.log('contact' + contactsIdValue)
+                console.log('contactNew' + contactsIdValue)
 
 
                 // fetch('https://proxy.cors.sh/https://api.ati.su/v2/cargos', {
