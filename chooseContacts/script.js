4f8c8aac0b1ccaae9511f28a61a359a553a8224b
nrodionov <nrodionov@i-novus.ru>
2023-04-26 21:53:46 +0300

    parse values from deal

--- chooseContacts/script.js
+++ chooseContacts/script.js
@@ -30,73 +30,30 @@ $(document).ready(function () {
         type: 'GET',
         success: function (data, status) {
             if (status === "success") {
-                let search_result = document.getElementById('search_box-result-contacts');
-
-                let table = '<div class="form-group col-sm-8">\n' +
-                    '        <label for="myMultiselect">Контакты</label>\n' +
-                    '        <div id="myMultiselect" class="multiselect">\n' +
-                    '            <div id="mySelectLabel" class="selectBox" onclick="toggleCheckboxArea()">\n' +
-                    '                <select class="form-select">\n' +
-                    '                    <option>Выбери из списка</option>\n' +
-                    '                </select>\n' +
-                    '                <div class="overSelect"></div>\n' +
-                    '            </div>\n' +
-                    '            <div id="mySelectOptions">';
+                let search_result = document.getElementById('contactsSelect');
+                // let table = '<select class="chosen" multiple="true" style="width:400px;">\n';
+                let table = ''
 
                 data.forEach(function (d) {
-                    table += '<label for="'+d.id+'"><input type="checkbox" id="'+d.id+'" onchange="checkboxStatusChange()" value="'+d.id+'" /> '+ d.name + '</label>'
-                })
-                table += '</div>\n' +
-                    '        </div>\n' +
-                    '<input type="button" onclick="saveContacts()" value="Сохранить контакты">'
-                    '    </div>';
+                        table += '<option> '+d.name + ' (' + d.id + ')' +' </option>'
+                    })
+
 
                 search_result.innerHTML = table;
+                console.log(search_result)
+                jQuery(".chosen").data("placeholder","Select Frameworks...").chosen();
+
             }
         }
     });
 
 });
 
-function checkboxStatusChange() {
-    var multiselect = document.getElementById("mySelectLabel");
-    var multiselectOption = multiselect.getElementsByTagName('option')[0];
-
-    var values = [];
-    var checkboxes = document.getElementById("mySelectOptions");
-    var checkedCheckboxes = checkboxes.querySelectorAll('input[type=checkbox]:checked');
-
-    for (const item of checkedCheckboxes) {
-        var checkboxValue = item.getAttribute('value');
-        values.push(checkboxValue);
-    }
-
-    var dropdownValue = "Nothing is selected";
-    if (values.length > 0) {
-        dropdownValue = values.join(', ');
-    }
-
-    multiselectOption.innerText = dropdownValue;
-}
-
-function toggleCheckboxArea(onlyHide = false) {
-    var checkboxes = document.getElementById("mySelectOptions");
-    var displayValue = checkboxes.style.display;
-
-    if (displayValue != "block") {
-        if (onlyHide == false) {
-            checkboxes.style.display = "block";
-        }
-    } else {
-        checkboxes.style.display = "none";
-    }
-}
-
 function saveContacts() {
-    var multiselect = document.getElementById("mySelectLabel");
-    var multiselectOption = multiselect.getElementsByTagName('option')[0];
+    var select = document.getElementById("contactsSelect");
+    var valueId = getSelectValues(select);
+    console.log(valueId);
     var fieldId = document.getElementById("fieldIdContacts").innerText;
-    var valueId = multiselectOption.innerText;
 
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
@@ -121,14 +78,28 @@ function saveContacts() {
     var w = window.EnvyCrmWidget
     w.changeDealValue({
         input_id: fieldId,
-        value: valueId
+        value: valueId.join(',')
     })
-        .then((data) => {
-            console.log(data);
+        .then(() => {
             alert('Контакты успешно обновлены')
         })
         .catch((e) => {
             console.log(e);
         });
+}
+
+function getSelectValues(select) {
+    var result = [];
+    var options = select && select.options;
+    var opt;
+
+    for (var i=0, iLen=options.length; i<iLen; i++) {
+        opt = options[i];
+
+        if (opt.selected) {
+            result.push(opt.value || opt.text);
+        }
+    }
+    return result;
 }
 
